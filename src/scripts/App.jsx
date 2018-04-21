9f6f011bc2fec639b1a6c22b8d43a504ef9af429
Tung Nguyen <kenkz447@gmail.com>
2018-04-21 16:47:20 +0700

    update

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,22 +1,12 @@
 import './App.scss'
 
 import React from 'react'
-import { Router, Switch, Route } from 'react-router'
-
-import { createBrowserHistory } from 'history'
-
 import { PageHome } from './pages'
-
-const browserHistory = createBrowserHistory()
 
 export function App(props) {
     return (
         <div className="app">
-            <Router history={browserHistory} >
-                <Switch>
-                    <Route path="/" component={PageHome}/>
-                </Switch>
-            </Router>
+            <PageHome/>
         </div>
     )
 }
